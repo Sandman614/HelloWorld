7fe570ccdb73922d27c8cc1b684fc1fcd2301fd7
TimsTop <TimsTop@TimsToy-PC>
2013-08-28 00:10:53 -0400

    first commit

---
+++ src/com/sandz/helloworld/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sandz.helloworld;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.option, menu);
+		new MenuInflater(this).inflate(R.menu.option, menu);
+		return (super.onCreateOptionsMenu(menu));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId()==R.id.action_settings) {
+			startActivity(new Intent(MainActivity.this,SecondActivity.class));
+			return(true);
+		}
+		return (super.onOptionsItemSelected(item));
+	}
+
+}
